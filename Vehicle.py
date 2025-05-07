288dcf607f2ad7932022b508d20388708aa23a71
SlimeDip <benedictborillo@gmail.com>
2025-05-07 19:53:57 +0800

    added class

--- Vehicle.py
+++ Vehicle.py
@@ -1,2 +1,9 @@
 # testing
 print("Hello World")
+
+class Vehicle:
+    def __init__(self, speed, maxfuel, durability, fuel_type):
+        self.speed = speed
+        self.maxfuel = maxfuel
+        self.durability = durability
+        self.fuel_type = fuel_type
