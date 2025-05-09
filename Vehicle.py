d5e6b942c9fe828273c24e995d5a6665408aa091
HeyOhJi <24-04907@g.batstate-u.edu.ph>
2025-05-09 21:06:06 +0800

    New Vehicles
    Added LandVehicles and WaterVehicles subclass
    Added Jetski and Speedboat
    Changed km/h to kn for WaterVehicles

--- Vehicle.py
+++ Vehicle.py
@@ -81,7 +81,29 @@ class Vehicle(ABC):
     def refuel(self, amount):
         pass
 
-class Car(Vehicle):
+class LandVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self._terrain = "land"
+
+class WaterVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self._terrain = "water"
+
+    @property
+    def speed(self):
+        return self._speed * 0.539957  # Convert km/h to knots
+
+    @speed.setter
+    def speed(self, value):
+        if value < 0:
+            self._speed = 60
+            print("Speed cannot be negative, setting to default value of 60 km/h.")
+        else:
+            self._speed = value
+
+class Car(LandVehicle):
     def start_engine(self):
         print("Car engine started")
 
@@ -98,7 +120,7 @@ class Car(Vehicle):
         self._fuel = self.max_fuel
         print(f"Car refueled: {self._fuel}L / {self.max_fuel}L")
 
-class Motorcycle(Vehicle):
+class Motorcycle(LandVehicle):
     def start_engine(self):
         print("Motorcycle engine started")
 
@@ -115,7 +137,7 @@ class Motorcycle(Vehicle):
         self._fuel = self.max_fuel
         print(f"Motorcycle refueled: {self._fuel}L / {self.max_fuel}L")
 
-class Bus(Vehicle):
+class Bus(LandVehicle):
     def start_engine(self):
         print("Bus engine started")
 
@@ -132,7 +154,7 @@ class Bus(Vehicle):
         self._fuel = self.max_fuel
         print(f"Bus refueled: {self._fuel}L / {self.max_fuel}L")
 
-class Truck(Vehicle):
+class Truck(LandVehicle):
     def start_engine(self):
         print("Truck engine started")
 
@@ -147,7 +169,41 @@ class Truck(Vehicle):
 
     def refuel(self):
         self._fuel = self.max_fuel
-        print(f"Bus refueled: {self._fuel}L / {self.max_fuel}L")
+        print(f"Truck refueled: {self._fuel}L / {self.max_fuel}L")
+
+class Speedboat(WaterVehicle):
+    def start_engine(self):
+        print("Speedboat engine started")
+
+    def stop_engine(self):
+        print("Speedboat engine stopped")
+
+    def drive(self, amount):
+        if self._fuel <= 0:
+            print("Cannot drive, fuel is empty")
+            return
+        self._fuel -= amount
+
+    def refuel(self):
+        self._fuel = self.max_fuel
+        print(f"Speedboat refueled: {self._fuel}L / {self.max_fuel}L")
+
+class Jetski(WaterVehicle):
+    def start_engine(self):
+        print("Jetski engine started")
+
+    def stop_engine(self):
+        print("Jetski engine stopped")
+
+    def drive(self, amount):
+        if self._fuel <= 0:
+            print("Cannot drive, fuel is empty")
+            return
+        self._fuel -= amount
+
+    def refuel(self):
+        self._fuel = self.max_fuel
+        print(f"Jetski refueled: {self._fuel}L / {self.max_fuel}L")
 
 def create_vehicle():
     print("Select vehicle type:")
@@ -155,6 +211,8 @@ def create_vehicle():
     print("2. Motorcycle")
     print("3. Bus")
     print("4. Truck")
+    print("5. Speedboat")
+    print("6. Jetski")
     vehicle_type = input("Enter your choice: ")
 
     if vehicle_type == "1":
@@ -165,6 +223,10 @@ def create_vehicle():
         x = Bus()
     elif vehicle_type == "4":
         x = Truck()
+    elif vehicle_type == "5":
+        x = Speedboat()
+    elif vehicle_type == "6":
+        x = Jetski()
     else:
         print("Invalid choice, returning to menu.")
         time.sleep(1)
@@ -177,8 +239,12 @@ def create_vehicle():
         x.fuel = x.max_fuel
         x.fuel_type = input("Enter vehicle fuel type (Gasoline, Diesel, Electric): ")
         x.speed = int(input("Enter vehicle speed (km/h): "))
-        Garage.append(x)
-        print(f"\n{x.name} has been added to the garage.")
+        if isinstance(x, LandVehicle):
+            Garage.append(x)
+            print(f"\n{x.name} has been added to the garage.")
+        elif isinstance(x, WaterVehicle):
+            Dock.append(x)
+            print(f"\n{x.name} has been added to the dock.")
     except ValueError:
         print("Invalid input, please enter numeric values for fuel and speed.")
         time.sleep(1)
@@ -204,6 +270,31 @@ def view_garage():
             clear()
             return
         CurrentVehicle = Garage[choice - 1]
+    except ValueError:
+        print("Invalid input, please enter a number.")
+        time.sleep(1)
+        clear()
+
+def view_dock():
+    global CurrentVehicle
+
+    print("-" * 20)
+    print(" D O C K")
+    for j, i in enumerate(Dock):
+        print("-" * 20)
+        print(f"{j+1}. {i.name}")
+        print(f"Vehicle type: {type(i).__name__}")
+        print(f"Vehicle fuel: {i.fuel} L | Fuel type: {i.fuel_type} | Speed: {i.speed:.2f} kn")
+    print("-" * 20)
+
+    try:
+        choice = int(input("\nSelect vehicle to drive (1, 2, ...): "))
+        if choice < 1 or choice > len(Dock):
+            print("Invalid choice.")
+            time.sleep(1)
+            clear()
+            return
+        CurrentVehicle = Dock[choice - 1]
     except ValueError:
         print("Invalid input, please enter a number.")
         time.sleep(1)
@@ -262,14 +353,17 @@ def drive_sim():
 
 clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
 
+Garage = []  # For LandVehicles
+Dock = []    # For WaterVehicles
+
 x = Car()
 x.name = "Family Car"
 x.fuel = 50
 x.max_fuel = 50
 x.fuel_type = "Gasoline"
 x.speed = 120
+Garage.append(x)
 
-Garage = [x]
 CurrentVehicle = Garage[0]
 
 if __name__ == "__main__":
@@ -279,8 +373,9 @@ if __name__ == "__main__":
         print("Select an option:")
         print("1. Create Vehicle")
         print("2. View Garage")
-        print("3. Drive Vehicle")
-        print("4. Exit")
+        print("3. View Dock")
+        print("4. Drive Vehicle")
+        print("5. Exit")
         choice = input("Enter your choice: ")
         
         if choice == "1":
@@ -294,6 +389,10 @@ if __name__ == "__main__":
             clear()
         elif choice == "3":
             clear()
+            view_dock()
+            clear()
+        elif choice == "4":
+            clear()
             print("Starting engine", end=" ", flush=True)
             for _ in range(4):
                 print(".", end=" ", flush=True)
@@ -305,7 +404,7 @@ if __name__ == "__main__":
             clear()
             drive_sim()
             clear()
-        elif choice == "4":
+        elif choice == "5":
             print("Exiting program.")
             break
         else:
